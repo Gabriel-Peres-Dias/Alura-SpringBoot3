a28e2032222c059fc6a83ac51f45e9f38bb8482f
Gabriel Peres <gabriel.suporte123@gmail.com>
2023-01-31 00:08:03 -0300

    Organizando os pacotes para melhor entendimento do que cada classe está fazendo. Criando classe DAO (repository) que extend a classe JpaRepository, classe que já temos as operações básicas de banco já prontas. Criamos a classe que representa o endereco com a anotação embeddable, para que os dados dela fique na mesma tabela que médico. Por fim fazemos o mapeamento padrão da entidade Medico.

--- src/main/java/med/vol/api/medico/dto/DadosCadastroMedico.java
+++ src/main/java/med/vol/api/medico/dto/DadosCadastroMedico.java
@@ -1,6 +1,7 @@
-package med.vol.api.medico;
+package med.vol.api.medico.dto;
 
 import med.vol.api.endereco.DadosEndereco;
+import med.vol.api.medico.enuns.Especialidade;
 
 //Utilizando as records do java, passando somente os atributos na classe
 public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
