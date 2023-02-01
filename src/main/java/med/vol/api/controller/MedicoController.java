f6462724861e7b6d3fbde067465306d6a682c260
Gabriel Peres <gabriel.2000mourad@gmail.com>
2023-01-31 21:20:05 -0300

    Adicionando uma paginação default de acordo com a rn. Habilitando o show sql para ver os sqls que estão sendo gerados.

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import med.vol.api.medico.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,7 +28,8 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+    //PageableDefault = Ao invés de usarmos a url, definimos o default da paginação, requisições com parametro na url sobrescrevem
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         //parser de entidade para dadosListagemMedico. Sendo necessário criar um construtor no dto que receba a entidade
         //método alterado para Page, e sem necessidade do .stream e .toList(), pois a Page já faz isso
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
