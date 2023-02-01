4f406d8eccac96c71e3bfb5fe2c17bf5ce508b97
Gabriel Peres <gabriel.suporte123@gmail.com>
2023-01-31 21:55:27 -0300

    Endpoint de exclusão física do registro pelo id.

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -43,4 +43,10 @@ public class MedicoController {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
     }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
 }
