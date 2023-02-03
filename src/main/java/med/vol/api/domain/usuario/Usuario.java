607983ec7780737da0f7ae8c8755316d1d074a47
Gabriel Peres <gabriel.suporte123@gmail.com>
2023-02-02 23:53:12 -0300

    Criando uma entidade do usuario que vai usar o sistema + criando a migration da tabela conforme a entidade.

---
+++ src/main/java/med/vol/api/domain/usuario/Usuario.java
@@ -0,0 +1,24 @@
+package med.vol.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+//criar o equals e hash encima do id, e não de todos os atributos
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+
+}
