1228e3db14d1f181e6bdcbc57a37e5b9f01a6d69
Gabriel Peres <gabriel.suporte123@gmail.com>
2023-02-03 00:01:23 -0300

    Criando a "DAO" do usuario com método de buscar o login do usuario + criando a service que vai implementar uma interface que o spring security utiliza.

---
+++ src/main/java/med/vol/api/domain/usuario/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package med.vol.api.domain.usuario.repository;
+
+import med.vol.api.domain.usuario.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByLogin(String login);
+}
