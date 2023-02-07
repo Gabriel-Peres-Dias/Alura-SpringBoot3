310323c6c65a65ba14a1da135cedcb38e90b8f9e
Gabriel Peres <gabriel.2000mourad@gmail.com>
2023-02-07 20:19:26 -0300

    Criando nossa classe de filtro para as requisições

---
+++ src/main/java/med/vol/api/infra/security/SecurityFilter.java
@@ -0,0 +1,22 @@
+package med.vol.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+// @component é utilizado para que o spring carregue uma classe/componente genérico
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+//filter chain representa a cadeia de filtros na aplicação, doFilter chama os próximos filtros da aplicação
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        filterChain.doFilter(request,response);
+    }
+}
