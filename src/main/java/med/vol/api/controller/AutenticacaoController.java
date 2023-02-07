365e4e019e53f1ed3aa09e8a5076965cdb62688b
Gabriel Peres <gabriel.2000mourad@gmail.com>
2023-02-07 19:37:00 -0300

    Criando nossa service relacionada ao token que vamos gerar, e adicionando o token gerado a response do método efetuarLogin.

--- src/main/java/med/vol/api/controller/AutenticacaoController.java
+++ src/main/java/med/vol/api/controller/AutenticacaoController.java
@@ -1,7 +1,9 @@
 package med.vol.api.controller;
 
 import jakarta.validation.Valid;
+import med.vol.api.domain.usuario.Usuario;
 import med.vol.api.domain.usuario.dto.DadosAutenticacao;
+import med.vol.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,11 +20,15 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
         var token = new UsernamePasswordAuthenticationToken(dados.login(),dados.senha());
         var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        //get principal seria a pessoa que está logando
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 }
