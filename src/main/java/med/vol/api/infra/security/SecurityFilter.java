ef83f0b641262e97c2b3b9c69632e017ff601141
Gabriel Peres <gabriel.suporte123@gmail.com>
2023-02-07 21:02:43 -0300

    recuperando o subject + correção num código onde ocorria erro por conta da versão do spring.

--- src/main/java/med/vol/api/infra/security/SecurityFilter.java
+++ src/main/java/med/vol/api/infra/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,19 +14,23 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private TokenService tokenService;
+
 //filter chain representa a cadeia de filtros na aplicação, doFilter chama os próximos filtros da aplicação
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
-        System.out.println(tokenJWT);
+        var subject = tokenService.getSubject(tokenJWT);
 
         filterChain.doFilter(request,response);
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authHeader = request.getHeader("Authorization");
-        if (authHeader == null) throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization.");
-
-        return authHeader.replace("Bearer", "");
+        if (authHeader != null) {
+            return authHeader.replace("Bearer ", "");
+        }
+        return null;
     }
 }
