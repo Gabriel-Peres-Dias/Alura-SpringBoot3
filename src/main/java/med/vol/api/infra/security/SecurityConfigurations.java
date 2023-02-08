e134ed43d7318abc23a28277afc440f8149c250e
Gabriel Peres <gabriel.suporte123@gmail.com>
2023-02-07 21:36:53 -0300

    terminando o projeto do curso + alterando as ordens dos filtros que o spring segue.

--- src/main/java/med/vol/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/vol/api/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package med.vol.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,16 +12,22 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 //Classe de configuração do spring security
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+    @Autowired
+    private SecurityFilter filter;
+
     //csrf = cross-site request forgery = como vamos usar tokens não precisamos dele pq ele o token já nos protege de ataques
     // SessionCreationPolicy.STATELESS = desabilitamos o processo de autenticação do spring do formulario, e como é api rest e rest é stateless
     // RequestMatchers = verbo http + url + permitAll(não preciso chegar se está previamente "logado")
     //anyRequest authenticated = todas as outras requisições precisam estar "logadas"
+    //AddFilterBefore = estamos dizendo para o spring a ordem dos filtros, que por padrão é o dele primeiro, e não queremos
+    //isso pq a autenticação está no nosso filtro
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
@@ -28,7 +35,8 @@ public class SecurityConfigurations {
                 .and().authorizeHttpRequests()
                 .requestMatchers(HttpMethod.POST, "/login").permitAll()
                 .anyRequest().authenticated()
-                .and().build();
+                .and().addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     // @Bean serve para exportar uma classe para o Spring, fazendo com que ele consiga carregá-la e realizar a sua injeção de dependência em outras classes.
