44397e5df63bf40a62a306a3272c6146e98baf72
Gabriel Peres <gabriel.2000mourad@gmail.com>
2023-02-07 21:02:43 -0300

    recuperando o subject + correção num código onde ocorria erro por conta da versão do spring.

--- src/main/java/med/vol/api/infra/security/TokenService.java
+++ src/main/java/med/vol/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.vol.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.vol.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -38,8 +39,23 @@ public class TokenService {
 
     }
 
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
+
+
     private Instant dataExpiracao() {
         //horário atual + duas horas, converter para instant com o fuso horário do Brasil
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
+
 }
