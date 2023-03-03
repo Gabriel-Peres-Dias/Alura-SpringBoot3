9b437036af4479b720340c5b3e22d047766f143b
Gabriel Peres <gabriel.suporte123@gmail.com>
2023-03-02 22:34:00 -0300

    Inicio da implementação da RN que verifica se o médico foi informado, caso não tenha, precisaremos escolher um de forma aleatória no banco.

--- src/main/java/med/vol/api/domain/consulta/service/ConsultaService.java
+++ src/main/java/med/vol/api/domain/consulta/service/ConsultaService.java
@@ -1,8 +1,10 @@
 package med.vol.api.domain.consulta.service;
 
+import med.vol.api.domain.ValidacaoException;
 import med.vol.api.domain.consulta.Consulta;
 import med.vol.api.domain.consulta.dto.DadosAgendamentoConsulta;
 import med.vol.api.domain.consulta.repository.ConsultaRepository;
+import med.vol.api.domain.medico.model.Medico;
 import med.vol.api.domain.medico.repository.MedicoRepository;
 import med.vol.api.domain.paciente.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,14 +23,23 @@ public class ConsultaService {
     private PacienteRepository pacienteRepository;
 
     public void agendar(DadosAgendamentoConsulta dados) {
+        if (!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("Id do paciente informado não existe!");
+        }
+
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
+            throw new ValidacaoException("Id do medico informado não existe!");
+        }
 
 
-
-
-        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var medico = escolherMedico(dados);
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
         var consulta = new Consulta(null, medico, paciente, dados.data());
 
         consultaRepository.save(consulta);
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+
     }
 }
