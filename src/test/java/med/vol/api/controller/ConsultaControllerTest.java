331b218bade96b804c59d90795011984e482988d
Gabriel Peres <gabriel.suporte123@gmail.com>
2023-03-07 21:48:47 -0300

    Criando o teste para o bad request da requisição de consultas.

---
+++ src/test/java/med/vol/api/controller/ConsultaControllerTest.java
@@ -0,0 +1,33 @@
+package med.vol.api.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ConsultaControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("Deveria devolver código 400 quando as informações estão inválidas")
+    @WithMockUser //mocka um usuario para ignora o spring security
+    void agendar_cenario1() throws Exception {
+        //when ou act -> quando disparar essa ação
+        var response = mvc.perform(post("/consultas"))
+        .andReturn().getResponse();
+
+        //then ou assert -> resultado esperado
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+}
